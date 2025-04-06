7ec9ce0f8dace22f4c41531c0dd4b4769833423f
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2025-04-06 14:17:22 +0200

    Modifying of file seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,31 +4,32 @@ import { faker } from "@faker-js/faker";
 const prisma = new PrismaClient();
 
 async function main() {
-    // ** Generate fake data for Todo model
-  await prisma.todo.createMany({
-    data: Array.from({ length: 25 }, () => ({
-      title: faker.lorem.words({ min: 2, max: 5 }),
-      body: faker.lorem.words({ min: 1, max: 10 }),
-    })),
-  });
-//   // Generate fake data for the User model
-//   const users = Array.from({ length: 25 }, () => ({
-//     email: faker.internet.email(),
-//     name: faker.internet.userName(),
-//     address: {
-//       street: faker.location.street(),
-//       city: faker.location.city(),
-//       state: faker.location.state(),
-//       zip: faker.location.zipCode(),
-//     },
-//   }));
-
-  
-  console.log("✅ 25 Fake Users Inserted Successfully!");
+  // ** Generate fake data for Todo model
+  // await prisma.todo.createMany({
+  //   data: Array.from({ length: 25 }, () => ({
+  //     title: faker.lorem.words({ min: 2, max: 5 }),
+  //     body: faker.lorem.words({ min: 1, max: 10 }),
+  //     // TODO replace the id below with your own user id after logging
+  //     user_id: "656422b1ce3129c5481f3710",
+  //   })),
+  // });
+  // ** Generate fake data for User model
+  // await prisma.user.createMany({
+  //   data: Array.from({ length: 25 }, () => ({
+  //     email: faker.internet.email(),
+  //     name: faker.internet.userName(),
+  //     address: {
+  //       street: faker.location.street(),
+  //       city: faker.location.city(),
+  //       state: faker.location.state(),
+  //       zip: faker.location.zipCode(),
+  //     },
+  //   })),
+  // });
 }
 
 main()
-  .catch(async (e) => {
+  .catch(async e => {
     console.error(e);
     process.exit(1);
   })
